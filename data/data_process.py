805fcaf19b6b5c1555b5c74ae252e1d30c3c9d65
Christer Söderholm <carl.christer.soderholm@gmail.com>
2022-11-27 23:34:03 +0200

    data things
    added all the split data and their analysis

--- data/data_process.py
+++ data/data_process.py
@@ -96,10 +96,12 @@ test5 = df_test5[["formula", "target"]].copy()
 train5 = df_train5[["formula", "target"]].copy()
 #%%
 """ Splitting the data randomly """
-# splitting with sklearn randomly might work since there are only unique formulas
-#X = df[['formula']]
-#y = df['target']
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=t_size, random_state=random_seed)
+
+df_train6 = df.sample(frac=1-t_size, random_state=(random_seed))
+df_test6 = df.drop(df_train6.index)
+
+test6 = df_test6[["formula", "target"]].copy()
+train6 = df_train6[["formula", "target"]].copy()
 
 #print("input\n")
 #print(X_train.head(10))
@@ -110,6 +112,37 @@ train5 = df_train5[["formula", "target"]].copy()
 #profile = ProfileReport(df, title="data profile")
 #profile.to_file("report.html")
 
+profileTest1 = ProfileReport(df_test1, title="test 1")
+profileTrain1 = ProfileReport(df_train1, title="train 1")
+profileTest1.to_file("test1.html")
+profileTrain1.to_file("train1.html")
+
+profileTest2 = ProfileReport(df_test2, title="test 2")
+profileTrain2 = ProfileReport(df_train2, title="train 2")
+profileTest2.to_file("test2.html")
+profileTrain2.to_file("train2.html")
+
+profileTest3 = ProfileReport(df_test3, title="test 3")
+profileTrain3 = ProfileReport(df_train3, title="train 3")
+profileTest3.to_file("test3.html")
+profileTrain3.to_file("train3.html")
+
+profileTest4 = ProfileReport(df_test4, title="test 4")
+profileTrain4 = ProfileReport(df_train4, title="train 4")
+profileTest4.to_file("test4.html")
+profileTrain4.to_file("train4.html")
+
+profileTest5 = ProfileReport(df_test5, title="test 5")
+profileTrain5 = ProfileReport(df_train5, title="train 5")
+profileTest5.to_file("test5.html")
+profileTrain5.to_file("train5.html")
+
+profileTest6 = ProfileReport(df_test6, title="test 6")
+profileTrain6 = ProfileReport(df_train6, title="train 6")
+profileTest6.to_file("test6.html")
+profileTrain6.to_file("train6.html")
+
+
 # get unique element in first spot of molecule
 #unique_molecule = df["A1"].unique()
 #print(f'{len(unique_molecule)} unique formulae:\n{unique_molecule}')
@@ -118,14 +151,14 @@ train5 = df_train5[["formula", "target"]].copy()
 #%%
 """ Saving data """
 # uncomment to save data again
-
+"""
 train_path1 = directory + '/train_data1.csv'
 test_path1 = directory + '/test_data1.csv'
 val_path1 = directory + '/val_data1.csv'
 train1.to_csv(train_path1, index=False)
 test1.to_csv(test_path1, index=False)
 val1.to_csv(val_path1, index=False)
-"""
+
 train_path2 = directory + '/train_data2.csv'
 test_path2 = directory + '/test_data2.csv'
 test2.to_csv(test_path2, index=False, header=None)
@@ -145,7 +178,13 @@ train_path5 = directory + '/train_data5.csv'
 test_path5 = directory + '/test_data5.csv'
 test5.to_csv(test_path5, index=False, header=None)
 train5.to_csv(train_path5, index=False, header=None)
+
+train_path6 = directory + '/train_data6.csv'
+test_path6 = directory + '/test_data6.csv'
+test6.to_csv(test_path6, index=False, header=None)
+train6.to_csv(train_path6, index=False, header=None)
 """
+
 # make reports for test and train data
 #profile_test = ProfileReport(df_test, title="data profile")
 #profile_test.to_file("test_report.html")
