2e699946a33d33a9b127cde7687f802f2652b964
WheatenTail <46076877+WheatenTail@users.noreply.github.com>
2022-12-02 15:26:31 +0200

    data better
    added changed training data and added validation data

--- data/data_process.py
+++ data/data_process.py
@@ -10,13 +10,13 @@ from pandas_profiling import ProfileReport
 random_seed = 458
 #np.random.seed(seed=random_seed)
 
-#val_size = 0.2
-t_size = 0.2
+val_size = 0.1
+test_size = 0.1
 #train_size = 1 - val_size - test_size
 
 # get the data from file
 directory = os.getcwd()
-data_path = directory + '/perovskite_data.xlsx'
+data_path = directory + r'/perovskite_data.xlsx'
 df = pd.read_excel(data_path, sheet_name="DFT Calculated Dataset")
 
 
@@ -61,46 +61,56 @@ train1 = df_train1[["formula", "target"]].copy()
 #%% set 2: only Pr, dy, Gd, Ho in the A site
 df_test2 = df[df["A1"].isin(second)]
 df_test2 = df_test2[df_test2["A2"].isin(second)]
-df_train2 = df[df["formula"].isin(df_test2["formula"])==False]
+df_val2 = df[df["formula"].isin(df_test2["formula"])==False].sample(round(df.shape[0]*0.1), random_state=42)
+df_train2 = df[df["formula"].isin(df_test2["formula"])==False & (df["formula"].isin(df_val2["formula"])==False)]
 
 test2 = df_test2[["formula", "target"]].copy()
+val2 = df_val2[["formula", "target"]].copy()
 train2 = df_train2[["formula", "target"]].copy()
 
 #%% set 3: only Ba AND Sr in A, and Fe in B (needs to have both acording to the size of the train data sets in the data paper)
 df_test3Fe = df[(df["B1"]=="Fe") | (df["B2"]=="Fe") | (df["B3"]=="Fe")] # all with Fe somewhere in B (1683 in excel has Fe in B1 and B2)
 df_test3 = df_test3Fe[ df_test3Fe["A1"].isin(third) & df_test3Fe["A2"].isin(third) & pd.isnull(df_test3Fe["A3"]) ] # all with Ba and Sr in A1 and A2
-df_train3 = df[df["formula"].isin(df_test3["formula"])==False]
+df_val3 = df[df["formula"].isin(df_test3["formula"])==False].sample(round(df.shape[0]*0.1), random_state=42)
+df_train3 = df[df["formula"].isin(df_test3["formula"])==False & (df["formula"].isin(df_val3["formula"])==False)]
 
 test3 = df_test3[["formula", "target"]].copy()
+val3 = df_val3[["formula", "target"]].copy()
 train3 = df_train3[["formula", "target"]].copy()
 #df_test3 = df_test3Fe[ df_test3Fe["A1"].isin(third) & pd.isnull(df_test3Fe["A2"]) ]    # I would say this is how they describe it in the paper, but that makes too many test elements
 
 #%% set 4: only V, Cr, Ti, Ga, or Sc atoms in B1 and B2
 df_test4 = df[df["B1"].isin(fourth)]
 df_test4 = df_test4[df_test4["B2"].isin(fourth)]
-df_train4 = df[df["formula"].isin(df_test4["formula"])==False]
+df_val4 = df[df["formula"].isin(df_test4["formula"])==False].sample(round(df.shape[0]*0.1), random_state=42)
+df_train4 = df[df["formula"].isin(df_test4["formula"])==False & (df["formula"].isin(df_val4["formula"])==False)]
 
 test4 = df_test4[["formula", "target"]].copy()
+val4 = df_val4[["formula", "target"]].copy()
 train4 = df_train4[["formula", "target"]].copy()
-#df_test4 = df[ (df["B1"].isin(fourth) & pd.isnull(df["B2"]) & pd.isnull(df["B3"]))    |
-           #    (df["B1"].isin(fourth) & df["B2"].isin(fourth) & pd.isnull(df["B3"]))  |
-              # (df["B1"].isin(fourth) & df["B2"].isin(fourth) & df["B3"].isin(fourth)) ]
+
 
 #%% set 5: one of the elements in the fifth list somewhere in the A spot
 df_test5 = df[ (df["A1"].isin(fifth) & ~(df["A2"].isin(fifth)) & ~(df["A3"].isin(fifth))) |
                (~(df["A1"].isin(fifth)) & df["A2"].isin(fifth) & ~(df["A3"].isin(fifth))) |
                (~(df["A1"].isin(fifth)) & ~(df["A2"].isin(fifth)) & df["A3"].isin(fifth))]
-df_train5 = df[df["formula"].isin(df_test5["formula"])==False]
+               
+df_val5 = df[df["formula"].isin(df_test5["formula"])==False].sample(round(df.shape[0]*0.1), random_state=42)
+df_train5 = df[df["formula"].isin(df_test5["formula"])==False & (df["formula"].isin(df_val5["formula"])==False)]
 
 test5 = df_test5[["formula", "target"]].copy()
+val5 = df_val5[["formula", "target"]].copy()
 train5 = df_train5[["formula", "target"]].copy()
 #%%
 """ Splitting the data randomly """
 
-df_train6 = df.sample(frac=1-t_size, random_state=(random_seed))
-df_test6 = df.drop(df_train6.index)
+df_test6 = df.sample(frac=test_size, random_state=(random_seed))
+df_val6 = df.drop(df_test6.index).sample(frac=val_size, random_state=(random_seed))
+df_train6 = df.drop(df_test6.index).drop(df_val6.index)
+
 
 test6 = df_test6[["formula", "target"]].copy()
+val6 = df_val6[["formula", "target"]].copy()
 train6 = df_train6[["formula", "target"]].copy()
 
 #print("input\n")
@@ -151,7 +161,7 @@ profileTrain6.to_file("train6.html")
 #%%
 """ Saving data """
 # uncomment to save data again
-"""
+
 train_path1 = directory + '/train_data1.csv'
 test_path1 = directory + '/test_data1.csv'
 val_path1 = directory + '/val_data1.csv'
@@ -161,34 +171,38 @@ val1.to_csv(val_path1, index=False)
 
 train_path2 = directory + '/train_data2.csv'
 test_path2 = directory + '/test_data2.csv'
-test2.to_csv(test_path2, index=False, header=None)
-train2.to_csv(train_path2, index=False, header=None)
+val_path2 = directory + '/val_data2.csv'
+test2.to_csv(test_path2, index=False)
+train2.to_csv(train_path2, index=False)
+val2.to_csv(val_path2, index=False)
 
 train_path3 = directory + '/train_data3.csv'
 test_path3 = directory + '/test_data3.csv'
-test3.to_csv(test_path3, index=False, header=None)
-train3.to_csv(train_path3, index=False, header=None)
+val_path3 = directory + '/val_data3.csv'
+test3.to_csv(test_path3, index=False)
+train3.to_csv(train_path3, index=False)
+val3.to_csv(val_path3, index=False)
 
 train_path4 = directory + '/train_data4.csv'
 test_path4 = directory + '/test_data4.csv'
-test4.to_csv(test_path4, index=False, header=None)
-train4.to_csv(train_path4, index=False, header=None)
+val_path4 = directory + '/val_data4.csv'
+test4.to_csv(test_path4, index=False)
+train4.to_csv(train_path4, index=False)
+val4.to_csv(val_path4, index=False)
 
 train_path5 = directory + '/train_data5.csv'
 test_path5 = directory + '/test_data5.csv'
-test5.to_csv(test_path5, index=False, header=None)
-train5.to_csv(train_path5, index=False, header=None)
+val_path5 = directory + '/val_data5.csv'
+test5.to_csv(test_path5, index=False)
+train5.to_csv(train_path5, index=False)
+val5.to_csv(val_path5, index=False)
 
 train_path6 = directory + '/train_data6.csv'
 test_path6 = directory + '/test_data6.csv'
-test6.to_csv(test_path6, index=False, header=None)
-train6.to_csv(train_path6, index=False, header=None)
-"""
+val_path6 = directory + '/val_data6.csv'
+test6.to_csv(test_path6, index=False)
+train6.to_csv(train_path6, index=False)
+val6.to_csv(val_path6, index=False)
 
-# make reports for test and train data
-#profile_test = ProfileReport(df_test, title="data profile")
-#profile_test.to_file("test_report.html")
 
-#profile_train = ProfileReport(df_train, title="data profile")
-#profile_train.to_file("train_report.html")
 print("done")
